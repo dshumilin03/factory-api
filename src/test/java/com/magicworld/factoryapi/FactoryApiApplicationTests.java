044978317f0e570f8b5740092fcddf134679d130
nxfear <Abelov772@bk.ru>
2024-02-15 17:22:04 +0300

    Created README file, initialized project with gradle

---
+++ src/test/java/com/magicworld/factoryapi/FactoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicworld.factoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
